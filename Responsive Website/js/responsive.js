90e10d1dec27679153b5d7c87dbd83ff39374d41
ahmadkhan5346 <khansarfraz5346@gmail.com>
2023-08-31 12:04:14 +0530

    analog clock

---
+++ Responsive Website/js/responsive.js
@@ -0,0 +1,11 @@
+// burger = document.querySelector('.burger')
+// navbar = document.querySelector('.navbar')
+// navList = document.querySelector('.nav-list')
+// rightNave = document.querySelector('.rightNave')
+
+
+// burger.addEventListner('click', ()=>{
+//     rightNave.classList.toggle('v-class-responsive');
+//     navList.classList.toggle('v-class-responsive');
+//     navbar.classList.toggle('h-nav-responsive');
+// })
